d9b0e898ae7723414b85c262885cb86f6e6c1025
Valentina Chambilla <vchambillap@unsa.edu.oe>
2025-05-19 11:33:39 -0500

    feat(main): Java project with files

---
+++ LAB5/XMLParserProject/src/parser/DOMParser.java
@@ -0,0 +1,28 @@
+package parser;
+
+import java.io.*;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+
+public class DOMParser {
+    public static void main(String[] args) {
+        try {
+            File xmlFile = new File("data/anexo2.xml");
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            factory.setValidating(true);
+            factory.setNamespaceAware(false);
+
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            builder.setErrorHandler(new org.xml.sax.helpers.DefaultHandler());
+            Document doc = builder.parse(xmlFile);
+
+            System.out.println("Documento XML válido.");
+            System.out.println("Elemento raíz: " + doc.getDocumentElement().getNodeName());
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
---
+++ LAB5/XMLParserProject/src/parser/SAXParserExample.java
@@ -0,0 +1,33 @@
+package parser;
+
+import javax.xml.parsers.*;
+import org.xml.sax.*;
+import org.xml.sax.helpers.*;
+import java.io.*;
+
+public class SAXParserExample extends DefaultHandler {
+    public static void main(String[] args) {
+        try {
+            File xmlFile = new File("data/anexo2.xml");
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            factory.setValidating(true);
+
+            SAXParser saxParser = factory.newSAXParser();
+            saxParser.parse(xmlFile, new SAXParserExample());
+
+            System.out.println("Documento XML válido.");
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) {
+        System.out.println("Inicio de elemento: " + qName);
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName) {
+        System.out.println("Fin de elemento: " + qName);
+    }
+}
